34ba2df0451fe4b82172d8a3fa48c3ae96204fe7
Dorian Calin <dorin.m.calin@gmail.com>
2020-03-31 08:45:37 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -10,8 +10,8 @@ end
 def loop_message_n_times(message,number)
   count = 0
   
-  while count < number.length do
-      puts message + number[count]
+  while count < number do
+      puts message 
       count += 1
   end
   
